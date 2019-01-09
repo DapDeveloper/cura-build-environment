6dff6999fbb8d77fbef781293aeaa80a97dde543
Lipu Fei <lipu.fei815@gmail.com>
2019-01-09 12:00:28 +0100

    WIP: Update packages

--- projects/python.cmake
+++ projects/python.cmake
@@ -77,7 +77,7 @@ if(NOT BUILD_OS_WINDOWS)
 
     # Numpy
     ExternalProject_Add_Step(Python add_numpy
-        COMMAND ${PYTHON_EXECUTABLE} -m pip install numpy==1.15.3
+        COMMAND ${PYTHON_EXECUTABLE} -m pip install numpy==1.15.4
         DEPENDEES upgrade_packages
     )
 
@@ -90,8 +90,8 @@ if(NOT BUILD_OS_WINDOWS)
 
     # Scipy
     ExternalProject_Add_Step(Python add_scipy
-        URL https://github.com/scipy/scipy/releases/download/v1.1.0/scipy-1.1.0.tar.gz
-        URL_MD5 aa6bcc85276b6f25e17bcfc4dede8718
+        URL https://github.com/scipy/scipy/releases/download/v1.2.0/scipy-1.2.0.tar.gz
+        URL_MD5 e57011507865b0b702aff6077d412e03
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${scipy_build_command}
         INSTALL_COMMAND ${scipy_install_command}
@@ -111,16 +111,16 @@ else()
     # Windows available depends on numpy with MKL, we also need the binary package for that.
     if(BUILD_OS_WIN32)
         ExternalProject_Add_Step(Python add_numpy_scipy_shapely
-            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/numpy-1.15.3+mkl-cp36-cp36m-win32.whl
-            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/scipy‑1.1.0‑cp36‑cp36m‑win32.whl
+            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/numpy-1.15.4+mkl-cp36-cp36m-win32.whl
+            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/scipy‑1.2.0‑cp36‑cp36m‑win32.whl
             COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/Shapely‑1.6.4.post1‑cp36‑cp36m‑win32.whl
             COMMENT "Install Numpy, Scipy, and Shapely"
             DEPENDEES upgrade_packages
         )
     else()
         ExternalProject_Add_Step(Python add_numpy_scipy_shapely
-            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/numpy-1.15.3+mkl-cp36-cp36m-win_amd64.whl
-            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/scipy‑1.1.0‑cp36‑cp36m‑win_amd64.whl
+            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/numpy-1.15.4+mkl-cp36-cp36m-win_amd64.whl
+            COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/scipy‑1.2.0‑cp36‑cp36m‑win_amd64.whl
             COMMAND ${PYTHON_EXECUTABLE} -m pip install http://software.ultimaker.com/cura-binary-dependencies/Shapely‑1.6.4.post1‑cp36‑cp36m‑win_amd64.whl
             COMMENT "Install Numpy, Scipy, and Shapely"
             DEPENDEES upgrade_packages
